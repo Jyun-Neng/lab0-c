e1968909e1126ef789fb3f0f60af3c2b47f0a282
jnji <jyunnengji@gmail.com>
2018-09-22 21:41:03 +0800

    Add fields in queue_t structure
    To record queue size and pointer to tail in queue.

--- queue.h
+++ queue.h
@@ -12,6 +12,7 @@
  */
 
 #include <stdbool.h>
+#include <stdio.h>
 
 /************** Data structure declarations ****************/
 
@@ -30,6 +31,8 @@ typedef struct {
                         You will need to add more fields to this structure
                         to efficiently implement q_size and q_insert_tail
                       */
+    list_ele_t *tail;
+    int size;
 } queue_t;
 
 /************** Operations on queue ************************/
